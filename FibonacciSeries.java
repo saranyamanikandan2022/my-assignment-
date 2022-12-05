f73f9e3b133f45a78337affdbafadd9ef6ec3c54
saranyamanikandan2022 <119424959+saranyamanikandan2022@users.noreply.github.com>
2022-12-05 22:23:28 +0530

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -19,20 +19,4 @@ System.out.println(firstTerm +" "+secondTerm);
 
   
 
-/*
-public static void main(String[] args) 
-{
-	int firstNum=0;
-	int secNum=1;
-	int sum;
-	
 
-	System.out.println(firstNum);
-	for(int i=1;i<=8;i++)
-	{
-		sum=firstNum+secNum;
-		System.out.println(sum);
-		firstNum=secNum;
-		secNum=sum;
-		
-	}*/
