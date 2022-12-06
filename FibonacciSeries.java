cc62c88835cac697586253a86f257dfc2533449f
saranyamanikandan2022 <119424959+saranyamanikandan2022@users.noreply.github.com>
2022-12-06 15:11:27 +0530

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -7,7 +7,7 @@ public class FibonacciSeries {
 		    int secondTerm=1;
 		    int sum;
 System.out.println(firstTerm);
-		 for(int i=2;i<=8;++i) //loop starts from 2 because 0 and 1 are already printed    
+		 for(int i=1;i<=9;i++ //loop starts from 2 because 0 and 1 are already printed    
 	 {    
 	  sum=firstTerm+secondTerm;    
 	  System.out.println(sum);    
