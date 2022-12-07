facaf31c8862ff721f328b18105ff4a067a920bc
saranyamanikandan2022 <119424959+saranyamanikandan2022@users.noreply.github.com>
2022-12-07 14:36:29 +0530

    Update DuplicateInArray.java

--- DuplicateInArray.java
+++ DuplicateInArray.java
@@ -8,9 +8,10 @@ public class DuplicateInArray {
         //Searches for duplicate element  
         for(int i = 0; i < arr.length; i++) {  
             for(int j = i + 1; j < arr.length; j++) {  
-                if(arr[i] == arr[j])  
+                if(arr[i] == arr[j])  {
                     System.out.println(arr[j]);  
             }  
         }  
     }  
-
+	}
+}
